69cad056d520d36826d2a4632bb38472a2466776
Hokoala <jean-michel.le@etudiant.univ-reims.fr>
2025-09-15 15:42:54 +0200

    FEAT : add graphql and api
    add graph and fix creatat on movie, category and actor

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -2,12 +2,14 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\CategoryRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 #[ORM\HasLifecycleCallbacks]
 #[ORM\Entity(repositoryClass: CategoryRepository::class)]
+#[ApiResource]
 class Category
 {
     #[ORM\Id]
@@ -77,11 +79,17 @@ class Category
     {
         return $this->createdAt;
     }
-    #[ORM\PrePersist]
+
     public function setCreatedAt(\DateTimeImmutable $createdAt): static
     {
         $this->createdAt = $createdAt;
 
         return $this;
+    }
+
+    #[ORM\PrePersist]
+    public function setCreatedAtValue(): void
+    {
+        $this->createdAt = new \DateTimeImmutable();
     }
 }
